a8ea590f819ac6271a0dab79b0c56350bc9c3727
Dataniel31 <alexdspe02@gmail.com>
2024-09-08 21:16:17 -0500

    botones añadidos

--- calculadora.py
+++ calculadora.py
@@ -4,21 +4,21 @@ import tkinter as tk
 ventana = tk.Tk()
 ventana.title('Mi Calculadora')
 
-#Cambiar icono de la calculadora
+# Cambiar icono de la calculadora
 ventana.iconbitmap("calculadora.ico")
 
-#configurar el tamaño dinamico de las columnas y filas
+# configurar el tamaño dinamico de las columnas y filas
 for i in range(5):
-    ventana.grid_rowconfigure(i,weight=1)
+    ventana.grid_rowconfigure(i, weight=1)
 
 for i in range(4):
-    ventana.grid_columnconfigure(i,weight=1)
+    ventana.grid_columnconfigure(i, weight=1)
 
-#cuadro de texto para mostrar las expressiones y resultados
+# cuadro de texto para mostrar las expressiones y resultados
 visor_texto = tk.StringVar()
 visor = tk.Entry(ventana,
                  textvariable=visor_texto,
-                 font=('Helvetica',24),
+                 font=('Helvetica', 24),
                  bd=10,
                  insertwidth=4,
                  width=14,
@@ -27,5 +27,33 @@ visor = tk.Entry(ventana,
 visor.grid(row=0,
            column=0,
            columnspan=4)
-#Ejecutar la aplicacion
+# Botones de la calculadora
+botones = [
+    ('7', 1, 0), ('8', 1, 1), ('9', 1, 2), ('/', 1, 3),
+    ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('*', 2, 3),
+    ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('-', 3, 3),
+    ('0', 4, 0), ('.', 4, 1), ('C', 4, 2), ('+', 4, 3),
+]
+# Crear y posicionar los botons (excepto "=")
+for (texto, fila, columna) in botones:
+    tk.Button(ventana,
+              text=texto,
+              padx=20,
+              pady=20,
+              font=('Helvetica', 20)
+              ).grid(row=fila,
+                     column=columna,
+                     sticky='nsew')
+
+# boton "=" ocupa una fila entera
+tk.Button(ventana,
+          text="=",
+          padx=20,
+          pady=20,
+          font=('Helvetica', 40)
+          ).grid(row=5,
+                 column=0,
+                 columnspan=4,
+                 sticky='nsew')
+# Ejecutar la aplicacion
 ventana.mainloop()
